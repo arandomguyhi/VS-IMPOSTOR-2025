80d2a2b06ecce4e10d8f045d2ca62eef549e9396
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2025-04-30 09:03:01 -0300

    BANANA 🍌 🍌

---
+++ scripts/global.lua
@@ -0,0 +1,120 @@
+luaDebugMode = true
+
+function onCreate()
+    runHaxeCode("createGlobalCallback('snapCamFollowToPos', function(x,y){parentLua.call('camSnap', [x,y]);});")
+
+    makeLuaSprite('flashSprite') makeGraphic('flashSprite', 1280, 720, 'b30000')
+    setProperty('flashSprite.alpha', 0)
+    setObjectCamera('flashSprite', 'camOther')
+    addLuaSprite('flashSprite', true)
+
+    createInstance('camPause', 'flixel.FlxCamera')
+    setProperty('camPause.bgColor', 0x0)
+
+    for _, cams in pairs({'camHUD', 'camOther'}) do
+        callMethodFromClass('flixel.FlxG', 'cameras.remove', {instanceArg(cams), false}) end
+    for _, cams in pairs({'camOther', 'camHUD', 'camPause'}) do -- yes, camOther is behind camHUD
+        callMethodFromClass('flixel.FlxG', 'cameras.add', {instanceArg(cams), false}) end
+end
+
+function camSnap(x,y)
+    callMethod('camFollow.setPosition', {x,y})
+    setCameraScroll(x,y)
+end
+
+function onEvent(eventName, value1, value2)
+    if eventName == 'Reactor Flash' then
+        if flashingLights then
+            setProperty('flashSprite.alpha', 0.3)
+            local flashDuration = tonumber(value1)
+            if type(flashDuration) ~= 'number' then
+                flashDuration = 0.5 end
+            doTweenAlpha('reactorTween', 'flashSprite', 0, flashDuration)
+        end
+    end
+
+    if eventName ~= 'camTween' then return end
+    if value1 == '' then
+        setProperty('isCameraOnForcedPos', false)
+        return
+    end
+
+    setProperty('isCameraOnForcedPos', true)
+    local coords = stringSplit(value1, ',')
+    local timing = stringSplit(value2, ',')
+
+    if value2 == '' and #coords >= 2 then
+       local x = tonumber(coords[1])
+       local y = tonumber(coords[2])
+       callMethod('camFollow.setPosition', {x, y})
+
+       if #coords == 3 then
+           local zoom = tonumber(coords[3])
+           setProperty('camGame.zoom', zoom)
+           setProperty('defaultCamZoom', zoom)
+       end
+       return
+    end
+
+    if #coords == 1 and #timing == 2 then
+        local leZoom, leTime, easingMethod = tonumber(value1), tonumber(timing[1]), tostring(timing[2])
+        startTween('zoomEvent', 'game', {['camGame.zoom'] = leZoom, defaultCamZoom = leZoom}, leTime, {ease = easingMethod})
+        return
+    end
+
+    if (#coords == 2 or #coords == 3) and #timing == 2 then
+        local x, y = tonumber(coords[1]), tonumber(coords[2])
+        local time, easingMethod = tonumber(timing[1]), tostring(timing[2])
+
+        startTween('camPosEvent', 'camFollow', {x = x, y = y}, time, {ease = easingMethod})
+        if #coords == 3 then
+            local zoom = tonumber(coords[3])
+            startTween('zoomEvent', 'game', {defaultCamZoom = zoom}, time, {ease = easingMethod})
+        end
+    end
+end
+
+function onCountdownStarted()
+    playSound('cancelMenu')
+
+    makeLuaSprite('count', 'get-ready')
+    addLuaSprite('count')
+    runHaxeCode("game.getLuaObject('count').camera = getVar('camPause');")
+
+    scaleObject('count', 0.5, 0.5)
+    screenCenter('count')
+
+    local oldY = getProperty('count.y')
+    setProperty('count.y', screenHeight)
+    startTween('counTween', 'count', {y = oldY}, crochet/1000, {ease = 'cubeInOut'})
+end
+
+function onCountdownTick(tick)
+    setProperty('countdownReady.visible', false)
+    setProperty('countdownSet.visible', false)
+    setProperty('countdownGo.visible', false)
+
+    local time = crochet/1000
+
+    if tick < 4 then
+        scaleObject('count', 0.55, 0.55, false)
+        startTween('countScale', 'count.scale', {x = 0.5, y = 0.5}, time/2, {})
+    end
+
+    if tick == 1 then
+        loadGraphic('count', 'ready')
+        updateHitbox('count')
+        screenCenter('count')
+    elseif tick == 2 then
+        loadGraphic('count', 'set')
+        updateHitbox('count')
+        screenCenter('count')
+    elseif tick == 3 then
+        loadGraphic('count', 'go')
+        updateHitbox('count')
+        screenCenter('count')
+    elseif tick == 4 then
+        startTween('byeCount', 'count', {y = screenHeight}, time, {ease = 'cubeInOut', onComplete = 'removeCount'})
+        function removeCount() removeLuaSprite('count', true) end
+    end
+end
---
+++ stages/dk.lua
@@ -0,0 +1,109 @@
+local ext = '../dk/stage/secret/'
+
+setProperty('skipArrowStartTween', true)
+
+function onCreate()
+    removeLuaScript('scripts/susHUD')
+
+    startVideo('banana', false, true, false, false)
+    setObjectCamera('videoCutscene', 'camHUD')
+
+    makeLuaSprite('bg', ext..'sky')
+
+    makeLuaSprite('ohioSprite') makeGraphic('ohioSprite', 1280, 720, 'ffffff')
+    setProperty('ohioSprite.alpha', 0)
+    setProperty('ohioSprite.camera', instanceArg('camPause'), false, true)
+    addLuaSprite('ohioSprite')
+
+    makeLuaSprite('bushes', ext..'skyass', 300, 225)
+    setScrollFactor('bushes', 0.75, 0.75)
+    scaleObject('bushes', 0.5, 0.5, false)
+
+    makeLuaSprite('stars', ext..'tree3', 0, -75)
+    setScrollFactor('stars', 0.8, 0.8)
+
+    makeLuaSprite('mountains', ext..'tree2', 0, -75)
+    setScrollFactor('mountains', 0.85, 0.85)
+
+    makeLuaSprite('mountains2', ext..'tree', 0, -75)
+    setScrollFactor('mountains2', 0.9, 0.9)
+
+    makeLuaSprite('floor', ext..'background')
+
+    setPropertyFromClass('substates.GameOverSubstate', 'characterName', 'diddy-dead')
+    setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'diddyscream')
+    setPropertyFromClass('substates.GameOverSubstate', 'loopSoundName', 'GameOverDK')
+    setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'GameOverDK_end')
+
+    createInstance('anotherCam', 'flixel.FlxCamera')
+    setProperty('anotherCam.bgColor', 0x0)
+    for _, cams in pairs({'camOther', 'camHUD', 'camPause'}) do
+        callMethodFromClass('flixel.FlxG', 'cameras.remove', {instanceArg(cams), false}) end
+    for _, cams in pairs({'camOther', 'camHUD', 'anotherCam', 'camPause'}) do
+        callMethodFromClass('flixel.FlxG', 'cameras.add', {instanceArg(cams), false}) end
+
+    makeLuaSprite('blackSprite') makeGraphic('blackSprite', 1280, 720, '000000')
+    setProperty('blackSprite.camera', instanceArg('anotherCam'), false, true)
+    addLuaSprite('blackSprite')
+
+    if shadersEnabled then
+        initLuaShader('rozebud')
+
+        makeLuaSprite('vhs') setSpriteShader('vhs', 'rozebud')
+        runHaxeCode([[
+            for (cams in [camHUD, camGame, getVar('camPause')])
+                cams.setFilters([new ShaderFilter(game.getLuaObject('vhs').shader)]);
+        ]])
+     end
+
+    for _, i in pairs({'bg', 'bushes', 'stars', 'mountains', 'mountains2', 'floor'}) do
+        updateHitbox(i)
+        scaleObject(i, 1.2, 1.2, false)
+        addLuaSprite(i)
+    end
+end
+
+function onCreatePost()
+    setPropertyFromClass('states.PlayState', 'SONG.arrowSkin', '../dk/noteskins/dkNotes')
+
+    for i = 0, 3 do
+        setProperty('opponentStrums.members['..i..'].alpha', 0)
+
+        setProperty('playerStrums.members['..i..'].scale.x', 0.7*4)
+        setProperty('playerStrums.members['..i..'].scale.y', 0.7*4)
+        updateHitboxFromGroup('playerStrums', i)
+
+        setProperty('playerStrums.members['..i..'].x', _G['defaultPlayerStrumX'..i] + 50)
+        setPropertyFromGroup('playerStrums', i, 'rgbShader.enabled', false)
+    end
+
+    for i = 0, getProperty('unspawnNotes.length')-1 do
+        setPropertyFromGroup('unspawnNotes', i, 'scale.x', 0.7*4)
+        setPropertyFromGroup('unspawnNotes', i, 'scale.y', 0.7*4)
+        updateHitboxFromGroup('unspawnNotes', i)
+
+        setPropertyFromGroup('unspawnNotes', i, 'rgbShader.enabled', false)
+        setPropertyFromGroup('unspawnNotes', i, 'noteSplashData.disabled', true)
+    end
+
+    for _, i in pairs({'comboGroup', 'scoreTxt', 'healthBar', 'iconP1', 'iconP2', 'timeBar', 'timeTxt'}) do
+        setProperty(i..'.visible', false)
+    end
+end
+
+function onSongStart()
+    startTween('unblack', 'blackSprite', {alpha = 0}, 0.05, {startDelay = 0.05})
+
+    callMethod('videoCutscene.play', {''})
+    setProperty('inCutscene', false)
+    setProperty('canPause', true)
+end
+
+function onEvent(eventName, value1, value2)
+    if eventName == 'orange' then
+        if value1 == 'dk' then
+            setProperty('camGame.zoom', getProperty('camGame.zoom') - 0.1)
+            startTween('noVideo', 'videoCutscene', {alpha = 0}, 0.6, {})
+        end
+    end
+end
